d16dccda0321cf3fc16eb8fb3eb65870c147223e
SaiKrishna1207 <saikrishna001207@gmail.com>
2021-04-19 20:12:15 +0530

    Refactoring

--- Python/nn_backprop.py
+++ Python/nn_backprop.py
@@ -3,17 +3,18 @@ import random
 import math
 import sys
 
-def loadFile(df):
+def loadFile(data):
   resultList = []
-  f = open(df, 'r')
+  f = open(data, 'r')
   for line in f:
-    line = line.rstrip('\n')  # "1.0,2.0,3.0"
-    sVals = line.split(',')   # ["1.0", "2.0, "3.0"]
-    fVals = list(map(np.float32, sVals))  # [1.0, 2.0, 3.0]
-    resultList.append(fVals)  # [[1.0, 2.0, 3.0] , [4.0, 5.0, 6.0]]
+    line = line.rstrip('\n')  
+    sVals = line.split(',')   
+    fVals = list(map(np.float32, sVals))  
+    resultList.append(fVals) 
   f.close()
-  return np.asarray(resultList, dtype=np.float32)  # not necessary
-	
+  npresults = np.asarray(resultList, dtype=np.float32)
+  return npresults
+
 def showMatrixPartial(m, numRows, dec, indices):
   fmt = "%." + str(dec) + "f" # like %.4f
   lastRow = len(m) - 1
@@ -28,18 +29,6 @@ def showMatrixPartial(m, numRows, dec, indices):
       x = m[i,j]
       if x >= 0.0: print(' ', end='')
       print(fmt % x + '  ', end='')
-    print('')
-  print(" . . . ")
-
-  if indices == True:
-    print("[", end='')
-    print(str(lastRow).rjust(width), end='')
-    print("] ", end='')	  
-  for j in range(len(m[lastRow])):
-    x = m[lastRow,j]
-    if x >= 0.0: print(' ', end='')
-    print(fmt % x + '  ', end='')
-  print('')	  
   
 class NeuralNetwork:
 
@@ -49,21 +38,21 @@ class NeuralNetwork:
     self.numOutputs = numOutput
 	
     self.InputNodes = np.zeros(shape=[self.numInputs], dtype=np.float32)
-    self.HiddenNodes = np.zeros(shape=[self.numHidden], dtype=np.float32)
-    self.OutputNodes = np.zeros(shape=[self.numOutputs], dtype=np.float32)
-	
     self.inputHiddenWeights = np.zeros(shape=[self.numInputs,self.numHidden], dtype=np.float32)
-    self.hiddenOutputWeights = np.zeros(shape=[self.numHidden,self.numOutputs], dtype=np.float32)
-	
     self.HiddenLayerBias = np.zeros(shape=[self.numHidden], dtype=np.float32)
+
+    self.HiddenNodes = np.zeros(shape=[self.numHidden], dtype=np.float32)
+    self.hiddenOutputWeights = np.zeros(shape=[self.numHidden,self.numOutputs], dtype=np.float32)
     self.OutputLayerBias = np.zeros(shape=[self.numOutputs], dtype=np.float32)
-	
+    self.OutputNodes = np.zeros(shape=[self.numOutputs], dtype=np.float32)
+		
     self.randomInitializer = random.Random(seed) # allows multiple instances
+    self.totalWeights = (self.numInputs * self.numHidden) + (self.numHidden * self.numOutputs) + self.numHidden + self.numOutputs
     self.initializeWeights()
  	
   def setWeights(self, weights):
-    if len(weights) != self.totalWeights(self.numInputs, self.numHidden, self.numOutputs):
-      print("Warning: len(weights) error in setWeights()")	
+    if len(weights) != self.totalWeights:
+      print("Weights number mismatch! Cannot set!")	
 
     idx = 0
     for i in range(self.numInputs):
@@ -71,80 +60,89 @@ class NeuralNetwork:
         self.inputHiddenWeights[i,j] = weights[idx]
         idx += 1
 		
-    for j in range(self.numHidden):
-      self.HiddenLayerBias[j] = weights[idx]
+    for i in range(self.numHidden):
+      self.HiddenLayerBias[i] = weights[idx]
       idx += 1
 
-    for j in range(self.numHidden):
-      for k in range(self.numOutputs):
-        self.hiddenOutputWeights[j,k] = weights[idx]
+    for i in range(self.numHidden):
+      for j in range(self.numOutputs):
+        self.hiddenOutputWeights[i,j] = weights[idx]
         idx += 1
 	  
-    for k in range(self.numOutputs):
-      self.OutputLayerBias[k] = weights[idx]
+    for i in range(self.numOutputs):
+      self.OutputLayerBias[i] = weights[idx]
       idx += 1
-	  
-  def getWeights(self):
-    tw = self.totalWeights(self.numInputs, self.numHidden, self.numOutputs)
-    result = np.zeros(shape=[tw], dtype=np.float32)
-    idx = 0  # points into result
-    
-    for i in range(self.numInputs):
-      for j in range(self.numHidden):
-        result[idx] = self.inputHiddenWeights[i,j]
-        idx += 1
-		
-    for j in range(self.numHidden):
-      result[idx] = self.HiddenLayerBias[j]
-      idx += 1
-
-    for j in range(self.numHidden):
-      for k in range(self.numOutputs):
-        result[idx] = self.hiddenOutputWeights[j,k]
-        idx += 1
-	  
-    for k in range(self.numOutputs):
-      result[idx] = self.OutputLayerBias[k]
-      idx += 1
-	  
-    return result
  	
   def initializeWeights(self):
-    numWts = self.totalWeights(self.numInputs, self.numHidden, self.numOutputs)
-    wts = np.zeros(shape=[numWts], dtype=np.float32)
-    lo = -0.01; hi = 0.01
-    for idx in range(len(wts)):
-      wts[idx] = (hi - lo) * self.randomInitializer.random() + lo
+    wts = [0] * self.totalWeights
+    wts = np.asarray(wts, dtype=np.float32)
+    low = -0.01 
+    high = 0.01
+    for idx in range(0, len(wts)):
+      wts[idx] = (high - low) * self.randomInitializer.random() + low
     self.setWeights(wts)
 
-  def computeOutputs(self, xValues): # A function to do forward propagation over the 3 layers of the neural network.E
+  def computeOutputs(self, xValues, epochnum): # A function to do forward propagation over the 3 layers of the neural network.
     hiddenLayerSums = np.zeros(shape=[self.numHidden], dtype=np.float32)
     outputLayerSums = np.zeros(shape=[self.numOutputs], dtype=np.float32)
 
     for i in range(self.numInputs):        # Input layer
       self.InputNodes[i] = xValues[i]   
 
+    if epochnum == 0: 
+      print("Input nodes: ")
+      print(self.InputNodes)
+
     for j in range(self.numHidden):        # Linear combination of the weights between input and hidden layers and the input neurons
       for i in range(self.numInputs):
         hiddenLayerSums[j] += self.InputNodes[i] * self.inputHiddenWeights[i,j]
 
+    if epochnum == 0:
+      print("Hidden layer sums before bias: ")
+      print(hiddenLayerSums)
+
     for j in range(self.numHidden):        # Adding the hidden layer bias
       hiddenLayerSums[j] += self.HiddenLayerBias[j]
+    if epochnum == 0:
+      print("Hidden layer sums after bias: ")
+      print(hiddenLayerSums)
 	  
     for j in range(self.numHidden):        # Activation function for hidden layer(TanH)
-      self.HiddenNodes[j] = self.hypertan(hiddenLayerSums[j])
+      interSum = hiddenLayerSums[j]
+      if interSum < -18.0:
+        self.HiddenNodes[j] = -1.0
+      elif interSum > 18.0:
+        self.HiddenNodes[j] = 1.0
+      else:
+        self.HiddenNodes[j] = math.tanh(interSum)
+
+    if epochnum == 0:
+      print("Hidden layer sums after activation: ")
+      print(self.HiddenNodes)
 
     for k in range(self.numOutputs):        # Linear combination of the weights between hidden and output layers and the hidden layer neurons
       for j in range(self.numHidden):
-        outputLayerSums[k] += self.HiddenNodes[j] * self.hiddenOutputWeights[j,k]
+        outputLayerSums[k] += ( self.HiddenNodes[j] * self.hiddenOutputWeights[j,k])
+
+    if epochnum == 0:
+      print("Output layer sums before bias: ")
+      print(outputLayerSums)
 
     for k in range(self.numOutputs):        # Adding output layer Bias
       outputLayerSums[k] += self.OutputLayerBias[k]
- 
+
+    if epochnum == 0:
+      print("Output layer sums after bias: ")
+      print(outputLayerSums)
+
     softmaxOutput = self.softmax(outputLayerSums)
     for k in range(self.numOutputs):        # Calculating Softmax output between the 3 classes 
       self.OutputNodes[k] = softmaxOutput[k]
-	  
+    
+    if epochnum == 0:
+      print("Output nodes after softmax: ")
+      print(self.OutputNodes)
+
     result = np.zeros(shape=self.numOutputs, dtype=np.float32)
     for k in range(self.numOutputs):
       result[k] = self.OutputNodes[k]
@@ -167,6 +165,16 @@ class NeuralNetwork:
     indices = np.arange(numTrainItems)          # [0, 1, 2, . . n-1]  # randomInitializer.shuffle(v)
 
     while epoch < maxEpochs:
+      if epoch == 0:
+        print("Input - Hidden Weights: ")
+        print(self.inputHiddenWeights)
+        print("Hidden - Output Weights: ")
+        print(self.hiddenOutputWeights)
+        print("Hidden bias: ")
+        print(self.HiddenLayerBias)
+        print("Output bias: ")
+        print(self.OutputLayerBias)
+      epochnum = epoch
       self.randomInitializer.shuffle(indices)   # scramble order of training items
       for ii in range(numTrainItems):
         idx = indices[ii]
@@ -174,24 +182,33 @@ class NeuralNetwork:
           x_values[j] = trainData[idx, j]             # get the input values	
         for j in range(self.numOutputs):
           t_values[j] = trainData[idx, j + self.numInputs]   # get the corresponding class values
-        self.computeOutputs(x_values)                 # results stored internally
-		
-    ### BACKPROPAGATION PHASE BEGINS 
+        self.computeOutputs(x_values, epoch)                 # results stored internally
+        epochnum = 1
 
+    ### BACKPROPAGATION PHASE BEGINS 
+        epochnum = epoch
         # Compute output node signals
         for k in range(self.numOutputs):
           derivative = (1 - self.OutputNodes[k]) * self.OutputNodes[k]
           outputSignals[k] = derivative * (self.OutputNodes[k] - t_values[k])
         
+
         # Compute hidden-to-output weight gradients using output signals
         for j in range(self.numHidden):
           for k in range(self.numOutputs):
             hiddenOutputGradients[j, k] = outputSignals[k] * self.HiddenNodes[j]
+
+        if(epochnum == 0):
+          print("Hidden - Output layer weight matrix gradient: ")
+          print(hiddenOutputGradients)
 			
         # Compute output node bias gradients using output signals
         for k in range(self.numOutputs):
           OutputBiasGradients[k] = outputSignals[k] * 1.0  # 1.0 dummy input can be dropped
-		  
+        if(epochnum == 0):
+          print("Output layer bias matrix gradient: ")
+          print(OutputBiasGradients)
+        
         # Compute hidden node signals
         for j in range(self.numHidden):
           sum = 0.0
@@ -205,111 +222,134 @@ class NeuralNetwork:
           for j in range(self.numHidden):
             InputHiddenGradients[i, j] = hiddenSignals[j] * self.InputNodes[i]
 
+        if(epochnum == 0):
+          print("Hidden - Input layer weight matrix gradient: ")
+          print(InputHiddenGradients)
+			
         # Compute hidden node bias gradients using hidden signals
         for j in range(self.numHidden):
           HiddenBiasGradients[j] = hiddenSignals[j] * 1.0  # 1.0 dummy input can be dropped
 
-        # Update weights and biases using the gradients
-		
+        if(epochnum == 0):
+          print("Hidden layer bias matrix gradient: ")
+          print(HiddenBiasGradients)
+
         # Update input-to-hidden weights
         for i in range(self.numInputs):
           for j in range(self.numHidden):
             delta = -1.0 * learnRate * InputHiddenGradients[i,j]
             self.inputHiddenWeights[i, j] += delta
-			
+
+        if epochnum == 0:
+          print("Delta value, i.e, deviation from initial value for the input-hidden weights: ")
+          print(delta)
+          print("Updated input-hidden weight matrix: ")
+          print(self.inputHiddenWeights)
+
         # Update hidden node biases
         for j in range(self.numHidden):
           delta = -1.0 * learnRate * HiddenBiasGradients[j]
           self.HiddenLayerBias[j] += delta      
+        if epochnum == 0:          
+          print("Delta value, i.e, deviation from initial value for the hidden layer biases: ")
+          print(delta)
+          print("Updated hidden layer biases")
+          print(self.HiddenLayerBias)
 		  
         # Update hidden-to-output weights
         for j in range(self.numHidden):
           for k in range(self.numOutputs):
             delta = -1.0 * learnRate * hiddenOutputGradients[j,k]
             self.hiddenOutputWeights[j, k] += delta
-			
+
+        if epochnum == 0:
+          print("Delta value, i.e, deviation from initial value for the hidden-output weights: ")
+          print(delta)
+          print("Updated hidden-output weight matrix: ")
+          print(self.hiddenOutputWeights)
         # Update output node biases
         for k in range(self.numOutputs):
           delta = -1.0 * learnRate * OutputBiasGradients[k]
           self.OutputLayerBias[k] += delta
 
+        if epochnum == 0:
+          print("Delta value, i.e, deviation from initial value for the output layer biases: ")
+          print(delta)
+          print("Updated output layer biases")
+          print(self.OutputLayerBias)
+        epochnum = 1
     ### BACKPROPAGATION PHASE ENDS
- 		  
+        
       epoch += 1
 	  
       if epoch % 10 == 0:
         mse = self.meanSquaredError(trainData)
         print("Epoch = " + str(epoch) + " Mean Squared Error = %0.4f " % mse)
-    # end while
-    
-    result = self.getWeights()
-    return result
   # end train
   
   def accuracy(self, tdata):  # train or test data matrix
-    num_correct = 0; num_wrong = 0
-    x_values = np.zeros(shape=[self.numInputs], dtype=np.float32)
-    t_values = np.zeros(shape=[self.numOutputs], dtype=np.float32)
+    correct_amt = 0
+    wrong_amt = 0
+    input_vals = [None] * self.numInputs
+    input_vals = np.asarray(input_vals, dtype=np.float32)
+    output_vals = [None] * self.numOutputs
+    output_vals = np.asarray(output_vals, dtype = np.float32)
 
-    for i in range(len(tdata)): 
-      for j in range(self.numInputs):  
-        x_values[j] = tdata[i,j]
-      for j in range(self.numOutputs): 
-        t_values[j] = tdata[i, j+self.numInputs]
+    for i in range(0, len(tdata)):
+      for j in range(0, self.numInputs):
+        input_vals[j] = tdata[i][j]
+      for j in range(0, self.numOutputs):
+        output_vals[j] = tdata[i][j + self.numInputs]
 
-      y_values = self.computeOutputs(x_values)  
-      max_index = np.argmax(y_values)  
+      predicted_vals = self.computeOutputs(input_vals, 1)  
+      mx = np.argmax(predicted_vals)  
 
-      if abs(t_values[max_index] - 1.0) < 1.0e-5:
-        num_correct += 1
+      deviation = abs(output_vals[mx] - 1.0)
+      if deviation >= 1e-3:
+        wrong_amt += 1
       else:
-        num_wrong += 1
+        correct_amt += 1
 
-    return (num_correct * 1.0) / (num_correct + num_wrong)
+    acc = correct_amt / (correct_amt + wrong_amt)
+    return acc
 
   def meanSquaredError(self, tdata):  # on train or test data matrix
-    sumSquaredError = 0.0
-    x_values = np.zeros(shape=[self.numInputs], dtype=np.float32)
-    t_values = np.zeros(shape=[self.numOutputs], dtype=np.float32)
 
-    for ii in range(len(tdata)):  # walk thru each data item
-      for jj in range(self.numInputs):  # peel off input values from curr data row 
-        x_values[jj] = tdata[ii, jj]
-      for jj in range(self.numOutputs):  # peel off target values from curr data row
-        t_values[jj] = tdata[ii, jj+self.numInputs]
+    initialSumError = 0.0
+    input_vals = [0] * self.numInputs
+    input_vals = np.asarray(input_vals, dtype=np.float32)
+    output_vals = [0] * self.numOutputs
+    output_vals = np.asarray(output_vals, dtype = np.float32)
 
-      y_values = self.computeOutputs(x_values)  # computed output values
-	  
-      for j in range(self.numOutputs):
-        err = t_values[j] - y_values[j]
-        sumSquaredError += err * err  # (t-o)^2
-		
-    return sumSquaredError / len(tdata)
-          
-  @staticmethod
-  def hypertan(x):
-    if x < -20.0:
-      return -1.0
-    elif x > 20.0:
-      return 1.0
-    else:
-      return math.tanh(x)
+    for i in range(0, len(tdata)):
+      for j in range(0, self.numInputs):
+        input_vals[j] = tdata[i][j]
+      for j in range(0, self.numOutputs):
+        output_vals[j] = tdata[i][j + self.numInputs]
+    
+    predicted_vals = self.computeOutputs(input_vals, 1)
+
+    for i in range(self.numOutputs):
+      error_squared = (output_vals[i] - predicted_vals[i])*(output_vals[i] - predicted_vals[i])
+      initialSumError += error_squared
+
+    initialSumError /= len(tdata)
+    return initialSumError
 
   @staticmethod	  
   def softmax(outputLayerSums):
-    result = np.zeros(shape=[len(outputLayerSums)], dtype=np.float32)
+    result = [0] * len(outputLayerSums)
+    result = np.asarray(result, dtype=np.float32)
     m = max(outputLayerSums)
     divisor = 0.0
-    for k in range(len(outputLayerSums)):
-       divisor += math.exp(outputLayerSums[k] - m)
-    for k in range(len(result)):
-      result[k] =  math.exp(outputLayerSums[k] - m) / divisor
+    exponents = []
+    for k in range(0, len(outputLayerSums)):
+      expo = math.exp(outputLayerSums[k] - m)
+      exponents.append(expo)
+      divisor += expo
+    for k in range(0, len(result)):
+      result[k] =  exponents[k] / divisor
     return result
-	
-  @staticmethod
-  def totalWeights(nInput, nHidden, nOutput):
-   tw = (nInput * nHidden) + (nHidden * nOutput) + nHidden + nOutput
-   return tw
 
 # end class NeuralNetwork
 
@@ -334,8 +374,6 @@ def main():
   
   maxEpochs = 50
   learnRate = 0.05
-  print("\nSetting maxEpochs = " + str(maxEpochs))
-  print("Setting learning rate = %0.3f " % learnRate)
   print("\nStarting training")
   nn.train(trainDataMatrix, maxEpochs, learnRate)
   print("Training complete")
@@ -343,8 +381,8 @@ def main():
   accTrain = nn.accuracy(trainDataMatrix)
   accTest = nn.accuracy(testDataMatrix)
   
-  print("\nAccuracy on 120-item train data = %0.4f " % accTrain)
-  print("Accuracy on 30-item test data   = %0.4f " % accTest)
+  print("\nAccuracy on train dataset = %0.4f " % accTrain)
+  print("Accuracy on test data   = %0.4f " % accTest)
      
 if __name__ == "__main__":
   main()
